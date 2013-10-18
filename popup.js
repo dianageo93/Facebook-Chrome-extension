b1c860ef2ff001feb170d8a058c694af2815b967
Ciocirdel Georgiana Diana <dianageo93@yahoo.com>
2013-10-18 11:16:40 +0300

    Added GoT messages and improved looks.

--- popup.js
+++ popup.js
@@ -1,96 +1,12 @@
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-/**
- * Global variable containing the query we'd like to pass to Flickr. In this
- * case, kittens!
- *
- * @type {string}
- */
-var QUERY = 'facebook';
-
-var kittenGenerator = {
-  /**
-   * Flickr URL that will give us lots and lots of whatever we're looking for.
-   *
-   * See http://www.flickr.com/services/api/flickr.photos.search.html for
-   * details about the construction of this URL.
-   *
-   * @type {string}
-   * @private
-   */
-  searchOnFlickr_: 'https://secure.flickr.com/services/rest/?' +
-      'method=flickr.photos.search&' +
-      'api_key=90485e931f687a9b9c2a66bf58a3861a&' +
-      'text=' + encodeURIComponent(QUERY) + '&' +
-      'safe_search=1&' +
-      'content_type=1&' +
-      'sort=interestingness-desc&' +
-      'per_page=20',
-
-  /**
-   * Sends an XHR GET request to grab photos of lots and lots of kittens. The
-   * XHR's 'onload' event is hooks up to the 'showPhotos_' method.
-   *
-   * @public
-   */
-  requestKittens: function() {
-    var req = new XMLHttpRequest();
-    req.open("GET", this.searchOnFlickr_, true);
-    req.onload = this.showPhotos_.bind(this);
-    req.send(null);
-  },
-
-  /**
-   * Handle the 'onload' event of our kitten XHR request, generated in
-   * 'requestKittens', by generating 'img' elements, and stuffing them into
-   * the document for display.
-   *
-   * @param {ProgressEvent} e The XHR ProgressEvent.
-   * @private
-   */
-  showPhotos_: function (e) {
-    var kittens = e.target.responseXML.querySelectorAll('photo');
-    for (var i = 0; i < kittens.length; i++) {
-      var img = document.createElement('img');
-      img.src = this.constructKittenURL_(kittens[i]);
-      img.setAttribute('alt', kittens[i].getAttribute('title'));
-      document.body.appendChild(img);
-    }
-  },
-
-  /**
-   * Given a photo, construct a URL using the method outlined at
-   * http://www.flickr.com/services/api/misc.urlKittenl
-   *
-   * @param {DOMElement} A kitten.
-   * @return {string} The kitten's URL.
-   * @private
-   */
-  constructKittenURL_: function (photo) {
-    return "http://farm" + photo.getAttribute("farm") +
-        ".static.flickr.com/" + photo.getAttribute("server") +
-        "/" + photo.getAttribute("id") +
-        "_" + photo.getAttribute("secret") +
-        "_s.jpg";
-  }
-};
-
-function pageScroll() {
-    	window.scrollBy(0,50); 
-    	scrolldelay = setTimeout('pageScroll()',100);
-}
+var myArray = ["Winter is coming. Speak your wish and stay warm.", "When you play a game of thrones you win or you die. Now speak your mind and let me be.", "Most men would rather deny a hard truth than face it. What would you make of me ?", "Death is so terribly final, while life is full of possibilities. What do you long for ?", "Nothing burns like the cold. I'm freezing here, so speak out !", "Every flight begins with a fall. So fly your wish and let me fall.", "What do we say to the Lord of Death?", "Summer will end soon enough, so tell me what you want.", "A lord must learn that sometimes words can accomplish what swords cannot. So what are your words ?", "A Lannister always pays his debts. How shall I pay mine ?", "A true man does what he will, not what he must. What is your will ?", "The Bear and the Maiden Fair, oh the Bear the Bear. Tell me your wish and I shall be the Maiden Fair."];
 
 document.addEventListener('DOMContentLoaded', function () {
-  //kittenGenerator.requestKittens();
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-	console.log(tabs[0]);
-	pageScroll();
-  });
-  setTimeout(functionToCall,5000);
+  chrome.browserAction.setIcon({path:"logo.png"});
+  var rand = myArray[Math.floor(Math.random() * myArray.length)];
+  document.getElementById("text").innerHTML = rand;
+  setTimeout(functionToCall,8000);
   function functionToCall() {
-  	//document.getElementById("box").innerHTML="whatever";
+  	chrome.browserAction.setIcon({path:"logo_charcoal.png"});
   	self.close();
   }
 });
